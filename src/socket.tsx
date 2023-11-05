808f0f2a0a34b4254349e5a24e3dab046fd900f9
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-05 22:06:20 +0000

    updated backend link

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // const URL = 'http://localhost:3000'
-const URL = 'https://math-tictactoe-backend-production.up.railway.app'
+const URL = 'https://math-tictactoe-backend-production.up.railway.app/'
 export const socket = io(URL, {
     autoConnect: false
 })
