0574de74e7efa05ba42b451486137a8c5db0e1ac
King Lazarus <vegetasssuper@gmail.com>
2023-11-07 22:48:14 +0000

    mobile button responsiveness

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,7 +4,7 @@ import Card from "./Card"
 const Player = () => {
   const data = useDataContext()
   return (
-    <section className="max-w-4xl mx-auto bg-gray-100 p-3 flex gap-2">
+    <section className="grid grid-cols-[repeat(auto-fit,_minmax(0,_1fr))] max-w-4xl m-2 md:mx-auto bg-gray-100 p-3 gap-2">
       {
         data?.playerCardData.map(card => (
           <Card key={card.id}  card={card}/>
